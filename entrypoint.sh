1efb6d3bf5f028c9cca8794172cd00f14fa09c15
Nils Gaehlert <nils.gaehlert@outlook.com>
2023-02-11 10:54:01 +0100

    initial commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 # entrypoint.sh
 
+echo " === NOW RUNNING EXTERNAL COMMANDS ==="
+/bin/sh -c "$@"
+
+echo " === NOW RUNNING SOME OTHER STUFF ==="
 echo "Hello World from within Docker entrypoint.sh"
 python --version
